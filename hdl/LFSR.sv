b37021dd469cb0521c133f8bd273c63bc03584d1
Benjamin Menkuec <benjamin@menkuec.de>
2023-01-25 11:14:45 +0100

    implement variable config loading

--- hdl/LFSR.sv
+++ hdl/LFSR.sv
@@ -4,32 +4,56 @@ module LFSR
 #(
     parameter N = 8,
     parameter START_VALUE = 8'b00000001,
-    parameter TAPS = 8'b00000011
+    parameter TAPS = 8'b00000011,
+    parameter VARIABLE_CONFIG = 0
 )
 (
     input                       clk_i,
     input                       reset_ni,
+    input                       load_config_i,
+    input   wire [N - 1 : 0]    taps_i,
+    input   wire [N - 1 : 0]    start_value_i,
     output  reg                 data_o
 );
 
 reg [N - 1 : 0] shift_reg;
 reg             newBit;
 
+// only needed when VARIABLE_CONFIG = 1, should get optimized away if VARIABLE_CONFIG = 0
+reg [N - 1 : 0] taps;
+
 always @(posedge clk_i) begin
     if (!reset_ni) begin
         data_o <= '0;
-        for (integer i = 0; i < N; i++) begin
-            shift_reg[i] <= (START_VALUE >> i) & 1'b1;
+        if (VARIABLE_CONFIG) begin
+            shift_reg <= '0;
+        end else begin    
+            for (integer i = 0; i < N; i++) begin
+                shift_reg[i] <= (START_VALUE >> i) & 1'b1;
+            end
         end
         // $display("%x", shift_reg);
     end else begin
         data_o <= shift_reg[0];
-        newBit = 0;
-        for (integer i = 0; i < N; i++) begin
-            newBit = newBit + (shift_reg[i] & ((TAPS >> i) & 1'b1));
+        if (VARIABLE_CONFIG) begin
+            if (load_config_i) begin
+                taps = taps_i;
+                shift_reg = start_value_i;
+                $display("taps = %d  shift_reg = %d\n\n\n", taps, shift_reg);
+            end
+            newBit = 0;
+            for (integer i = 0; i < N; i++) begin
+                newBit = newBit + (shift_reg[i] & taps[i]);
+            end
+            shift_reg <= {newBit, shift_reg[N - 1 : 1]};            
+        end else begin
+            newBit = 0;
+            for (integer i = 0; i < N; i++) begin
+                newBit = newBit + (shift_reg[i] & ((TAPS >> i) & 1'b1));
+            end
+            // [R_(N-1) .... R_0] -> [newBit R_(N-1) .... R_1]
+            shift_reg <= {newBit, shift_reg[N - 1 : 1]};
         end
-        // [R_(N-1) .... R_0] -> [newBit R_(N-1) .... R_1]
-        shift_reg <= {newBit, shift_reg[N - 1 : 1]};
     end
 end
 
--- tests/test_LFSR.py
+++ tests/test_LFSR.py
@@ -76,10 +76,38 @@ async def _test_parameter(dut):
     while count < num_items:
         await RisingEdge(dut.clk_i)
         received[count] = int(tb.dut.data_o)
-        print(f"{received[count]} <-> {model[count]}")
+        # print(f"{received[count]} <-> {model[count]}")
         assert model[count] == received[count]
         count += 1
-# cocotb-test
+
+@cocotb.test()
+async def _test_variable_config(dut):
+    tb = TB(dut)
+
+    # do reset and immediately load config
+    dut.reset_ni.value = 0
+    await RisingEdge(dut.clk_i)
+    dut.reset_ni.value = 0
+    await RisingEdge(dut.clk_i)
+    dut.reset_ni.value = 1
+    dut.load_config_i.value = 1
+    # load start values for PSS(0)
+    dut.start_value_i.value = 0x76
+    dut.taps_i.value = 0x11
+    await RisingEdge(dut.clk_i)
+    dut.load_config_i.value = 0
+
+    count = 0
+    num_items = 127
+    # model = py3gpp.helper._calc_m_seq(tb.N, _int_to_vector(tb.START_VALUE, tb.N), _int_to_taps(tb.TAPS, tb.N), num_items)
+    model = py3gpp.nrPSS(0)
+    received = np.empty(num_items, int)
+    while count < num_items:
+        await RisingEdge(dut.clk_i)
+        received[count] = int(tb.dut.data_o)
+        # print(f"{received[count]} <-> {model[count]}")
+        assert model[count] == received[count]
+        count += 1        
 
 
 tests_dir = os.path.abspath(os.path.dirname(__file__))
@@ -89,7 +117,7 @@ tools_dir = os.path.abspath(os.path.join(tests_dir, '..', 'tools'))
 @pytest.mark.parametrize("N", [7])
 @pytest.mark.parametrize("START_VALUE", [0x76, 0x00])
 @pytest.mark.parametrize("TAPS", [0x11, 0x7F, 0x40])
-def test(N, START_VALUE, TAPS):
+def test_parameter(N, START_VALUE, TAPS):
     dut = "LFSR"
     module = os.path.splitext(os.path.basename(__file__))[0]
     toplevel = dut
@@ -107,7 +135,6 @@ def test(N, START_VALUE, TAPS):
     parameters['START_VALUE'] = START_VALUE
     parameters['TAPS'] = TAPS
     
-    extra_env = {f'PARAM_{k}': str(v) for k, v in parameters.items()}
     sim_build="sim_build/" + "_".join(("{}={}".format(*i) for i in parameters.items()))
     cocotb_test.simulator.run(
         python_search=[tests_dir],
@@ -117,14 +144,46 @@ def test(N, START_VALUE, TAPS):
         module=module,
         parameters=parameters,
         sim_build=sim_build,
-        extra_env=extra_env,
         testcase="_test_parameter",
     )
 
+@pytest.mark.parametrize("N", [7])
+@pytest.mark.parametrize("VARIABLE_CONFIG", [1])
+def test_variable_config(N, VARIABLE_CONFIG):
+    dut = "LFSR"
+    module = os.path.splitext(os.path.basename(__file__))[0]
+    toplevel = dut
+
+    verilog_sources = [
+        os.path.join(rtl_dir, f"{dut}.sv"),
+    ]
+    includes = [
+        os.path.join(rtl_dir, ""),
+    ]    
+
+    parameters = {}
+
+    parameters['N'] = N
+    parameters['VARIABLE_CONFIG'] = VARIABLE_CONFIG
+
+    sim_build="sim_build/" + "_".join(("{}={}".format(*i) for i in parameters.items()))
+    cocotb_test.simulator.run(
+        python_search=[tests_dir],
+        verilog_sources=verilog_sources,
+        includes=includes,
+        toplevel=toplevel,
+        module=module,
+        parameters=parameters,
+        sim_build=sim_build,
+        testcase="_test_variable_config",
+    )
+
 if __name__ == '__main__':
+    test_variable_config(N = 7, VARIABLE_CONFIG = 1)
+
     START_VALUE = np.array([1, 1, 1, 0, 1, 1, 0]) # bits here are in reversed order!
     ncellid = 0
     START_VALUE = np.roll(START_VALUE, -43 * ncellid)
-    test(N = 7, START_VALUE = int(''.join(map(str, START_VALUE)), 2), TAPS = 0x11)
+    test_parameter(N = 7, START_VALUE = int(''.join(map(str, START_VALUE)), 2), TAPS = 0x11)
 
     
