f3a184be900ef7de4796ea2a304726be9c00ea04
Benjamin Menkuec <benjamin@menkuec.de>
2023-01-25 10:36:34 +0100

    improved test

--- tests/test_LFSR.py
+++ tests/test_LFSR.py
@@ -52,14 +52,26 @@ class TB(object):
         await RisingEdge(self.dut.clk_i)
         # self.model.reset()
         
+def _int_to_vector(val, len):
+    vec = np.empty(len, int)
+    for i in range(len):
+        vec[i] = (val >> i) & 1
+    return vec
+
+def _int_to_taps(val, len):
+    taps = list()
+    for i in range(len):
+        if (val >> i) & 1:
+            taps.append(i)
+    return np.array(taps)
         
 @cocotb.test()
-async def simple_test(dut):
+async def _test_parameter(dut):
     tb = TB(dut)
     await tb.cycle_reset()
     count = 0
-    model = py3gpp.nrPSS(0)
-    num_items = len(model)
+    num_items = 2**tb.N + 10 # +10 to include some of the sequence repetition in the test
+    model = py3gpp.helper._calc_m_seq(tb.N, _int_to_vector(tb.START_VALUE, tb.N), _int_to_taps(tb.TAPS, tb.N), num_items)
     received = np.empty(num_items, int)
     while count < num_items:
         await RisingEdge(dut.clk_i)
@@ -74,9 +86,9 @@ tests_dir = os.path.abspath(os.path.dirname(__file__))
 rtl_dir = os.path.abspath(os.path.join(tests_dir, '..', 'hdl'))
 tools_dir = os.path.abspath(os.path.join(tests_dir, '..', 'tools'))
 
-@pytest.mark.parametrize("N", [8])
-@pytest.mark.parametrize("START_VALUE", [7])
-@pytest.mark.parametrize("TAPS", [7])
+@pytest.mark.parametrize("N", [7])
+@pytest.mark.parametrize("START_VALUE", [0x76, 0x00])
+@pytest.mark.parametrize("TAPS", [0x11, 0x7F, 0x40])
 def test(N, START_VALUE, TAPS):
     dut = "LFSR"
     module = os.path.splitext(os.path.basename(__file__))[0]
@@ -106,12 +118,11 @@ def test(N, START_VALUE, TAPS):
         parameters=parameters,
         sim_build=sim_build,
         extra_env=extra_env,
-        testcase="simple_test",
+        testcase="_test_parameter",
     )
 
 if __name__ == '__main__':
-    os.environ['PLOTS'] = "1"
-    START_VALUE = np.array([1, 1, 1, 0, 1, 1, 0])
+    START_VALUE = np.array([1, 1, 1, 0, 1, 1, 0]) # bits here are in reversed order!
     ncellid = 0
     START_VALUE = np.roll(START_VALUE, -43 * ncellid)
     test(N = 7, START_VALUE = int(''.join(map(str, START_VALUE)), 2), TAPS = 0x11)
